d9febaf6a4b52269b119e409c597d6d6c1378904
Ismaelvg-educarex <ivalverdeg01@educarex.es>
2026-05-23 21:23:26 +0200

    Funciona admin,pero con alert
    Refactor admin user check and add alert for non-admins.

--- API_Taller/js/admin.js
+++ API_Taller/js/admin.js
@@ -4,8 +4,9 @@ const datosUsuario = localStorage.getItem("usuario");
 if (!token || !datosUsuario) {
     window.location.replace("index.html");
 } else {
-    const usuario = JSON.parse(datosUsuario);
-    if (usuario.tipoUsuario !== "admin") {
+    const esAdmin = usuario.tipoUsuario == "admin";
+    if (!esAdmin) {
+        alert('Buen intento pero no eres admin, la proxima vez katanazo en la cara');
         window.location.replace("index.html");
     } else {
         document.body.style.display = "block";
